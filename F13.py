147c24bf6ad2b26c974e628767215781bfe2ace1
Hifzhi Dinullah <hifzhidinullah@gmail.com>
2023-04-14 10:30:50 +0700

    perubahan cek valid argparse

--- F13.py
+++ F13.py
@@ -2,14 +2,17 @@ from additional import splitter
 
 def valid(folder):
     try:
-        with open(f'{folder}/user.csv', 'r') as f:
-            print('''Loading...
-    Selamat datang di program “Manajerial Candi”
-    Silahkan masukkan username Anda''')
-            return True
-    except FileNotFoundError:
+        f = open(f'{folder}/user.csv', 'r')
+        f.close()
+        print('''Loading...
+
+Selamat datang di program “Manajerial Candi”
+Silahkan masukkan username Anda''')
+        return True
+    except IOError:
         if folder == "":
             print('''Tidak ada nama folder yang diberikan!
+
 Usage: python main.py <nama_folder>''')
         else :
             print(f'Folder "{folder}" tidak ditemukan.')
